095859f6267e2b9fe7dd8acccea4ff942cca853e
KTo <kto@lovetou.ru>
2022-12-12 23:34:35 +0700

    Поиск отображений

--- savraska/exceptions.py
+++ savraska/exceptions.py
@@ -1,3 +1,3 @@
-class NotFound(Exception):
+class PageNotFound(Exception):
     code = 404
     text = 'Страница не найдена'
--- savraska/main.py
+++ savraska/main.py
@@ -1,5 +1,8 @@
+import re
 from typing import List
+
 from savraska.urls import Url
+from savraska.exceptions import PageNotFound
 
 
 class Savraska:
@@ -17,3 +20,16 @@ class Savraska:
         start_response('200 OK', [('Content-Type', 'text/html')])
         # возвращаем тело ответа в виде списка из bite
         return [b'Hello world from a simple WSGI application!']
+
+    def __prepare_url(self, url: str):
+        return url if url.endswith('/') else f'{url}/'
+
+    def __find_view(self, raw_url: str):
+        url = self.__prepare_url(raw_url)
+
+        for path in self.urls:
+            m = re.match(path.url, url)
+            if m is not None:
+                return path.view
+
+        raise PageNotFound
